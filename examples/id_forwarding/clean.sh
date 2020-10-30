4f47389cfc2c3680a1acec79461a5cc5b4453713
Vishal Shrivastav <vishal.iitkgp08@gmail.com>
2020-10-29 21:38:44 -0400

    added controller files

--- examples/id_forwarding/clean.sh
+++ examples/id_forwarding/clean.sh
@@ -8,3 +8,6 @@ sudo ip link del b1-tap1
 sudo ip link del b2-tap1
 sudo ip link del b3-tap1
 sudo ip link del b4-tap1
+
+#remove the executable
+rm controller/controller_client
--- examples/id_forwarding/create_vms.sh
+++ examples/id_forwarding/create_vms.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-echo "Starting 4 twizzler VMs.."
-cd /home/vshrivastav/twizzler  #location of twizzler folder on the system
-sudo PROJECT=x86_64 QEMU_FLAGS='-display none' make all test INSTANCES="t,b1-tap1,08:00:00:00:01:11 t,b2-tap1,08:00:00:00:02:22 t,b3-tap1,08:00:00:00:03:33 t,b4-tap1,08:00:00:00:04:44"
+opt=$1
+
+if [ $# != 1 ]
+then
+    echo "Wrong number of arguments. Usage -- ./create_vms.sh [linux/twizzler]"
+    exit
+fi
+
+if [[ $opt = "linux" ]]
+then
+    echo "Starting 1 linux VM.."
+    sudo qemu-system-x86_64 -nographic -kernel images/bzImage -hda images/wheezy.img -append "root=/dev/sda console=ttyS0" -netdev user,id=net0,hostfwd=tcp::2222-:22 -device e1000,netdev=net0 -netdev tap,id=net1,ifname=b4-tap1,script=no,downscript=no -device e1000,netdev=net1,mac='08:00:00:00:04:44' -name vm4
+
+elif [[ $opt = "twizzler" ]]
+then
+    echo "Starting 3 twizzler VMs.."
+    cd /home/vshrivastav/twizzler  #location of twizzler folder on the system
+    sudo PROJECT=x86_64 QEMU_FLAGS='-display none' make all test INSTANCES="t,b1-tap1,08:00:00:00:01:11 t,b2-tap1,08:00:00:00:02:22 t,b3-tap1,08:00:00:00:03:33"
+
+else
+    echo "Unrecognized command line argument. It must be linux or twizzler"
+fi
